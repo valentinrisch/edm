0c7aefb58a9f4364bfc208abe71d43218f866d8c
s74593 <s74593@beuth-hochschule.de>
2018-10-20 20:06:18 +0200

    wip

--- api/app/models.py
+++ api/app/models.py
@@ -1,6 +1,10 @@
 from app import db
 
 
-class User(db.Model):
+class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     comment = db.Column(db.String(64), index=True, unique=True)
+    toxic = db.Column(db.String(64))
+
+    def __repr__(self):
+        return '<Comment {}>'.format(self.comment)
